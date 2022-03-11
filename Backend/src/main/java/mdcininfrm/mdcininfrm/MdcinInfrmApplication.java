b489d533c5541998de5e26037c367b795466a555
YongWoo Kim <ywkim@wkit.co.kr>
2022-03-11 09:26:08 +0900

    프로젝트 생성

---
+++ Backend/src/main/java/mdcininfrm/mdcininfrm/MdcinInfrmApplication.java
@@ -0,0 +1,13 @@
+package mdcininfrm.mdcininfrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdcinInfrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdcinInfrmApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/mdcininfrm/mdcininfrm/MdcinInfrmApplicationTests.java
@@ -0,0 +1,13 @@
+package mdcininfrm.mdcininfrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdcinInfrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
