1168d9ce669d5464da042e73d8d73725a4ea6653
yeogini <ywkim@wkit.co.kr>
2022-03-11 14:37:04 +0900

    스웨거 추가

---
+++ Backend/src/main/java/mdcininfrm/mdcininfrm/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package mdcininfrm.mdcininfrm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket restAPI() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("mdcininfrm.mdcininfrm.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("MdcinInfrm")
+                .version("1.0")
+                .description("")
+                .build();
+    }
+
+}
---
+++ Backend/src/main/java/mdcininfrm/mdcininfrm/controller/RestApiController.java
@@ -0,0 +1,7 @@
+package mdcininfrm.mdcininfrm.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApiController {
+}
