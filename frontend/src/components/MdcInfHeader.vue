6ea42a774566ea4e5daa4d20a86c67e39a7d26ae
JunYoung <ko5642027@naver.com>
2022-03-16 22:01:41 +0900

    헤더추가

---
+++ frontend/src/components/MdcInfHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>호흡기 진료 지정 의료기관 정보 서비스</h1>
+  </div>
+</template>
---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <MdcInfHeader></MdcInfHeader>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import MdcInfHeader from "@/components/MdcInfHeader.vue";
+
+export default Vue.extend({
+  name: "MainView",
+  components: {
+    MdcInfHeader,
+  },
+});
+</script>
